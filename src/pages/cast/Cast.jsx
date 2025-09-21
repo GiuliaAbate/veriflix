edbd169c13b211c198efe2723221342fa9e13b74
Giulia Abate <giulia.abate1101@gmail.com>
2025-09-21 14:06:27 +0200

    first obj in cast page

--- src/pages/cast/Cast.jsx
+++ src/pages/cast/Cast.jsx
@@ -1,5 +1,88 @@
 function Cast() {
-  return <div>Cast Page</div>;
+
+  const verivery = [
+    {
+      'id': 1,
+      'pfp': '',
+      'name': '',
+      'role': '',
+      'birthday': '',
+      'facts': {
+
+      }
+    },
+    {
+      'id': 2,
+      'pfp': '',
+      'name': '',
+      'role': '',
+      'birthday': '',
+      'facts': {
+
+      }
+    },
+    {
+      'id': 3,
+      'pfp': '',
+      'name': '',
+      'role': '',
+      'birthday': '',
+      'facts': {
+
+      }
+    },
+    {
+      'id': 4,
+      'pfp': '',
+      'name': '',
+      'role': '',
+      'birthday': '',
+      'facts': {
+
+      }
+    },
+    {
+      'id': 5,
+      'pfp': '',
+      'name': '',
+      'role': '',
+      'birthday': '',
+      'facts': {
+
+      }
+    },
+    {
+      'id': 6,
+      'pfp': '',
+      'name': '',
+      'role': '',
+      'birthday': '',
+      'facts': {
+
+      }
+    },
+    {
+      'id': 7,
+      'pfp': '',
+      'name': '',
+      'role': '',
+      'birthday': '',
+      'facts': {
+
+      }
+    }
+  ]
+
+  return (
+    <>
+      <div>Cast Page</div>
+      {verivery.map(vrvr => (
+        <>
+          {vrvr}
+        </>
+      ))};
+    </>
+  );
 }
 
 export default Cast;
