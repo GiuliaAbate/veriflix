6e81916813e5d83dccbe27573f7c859770bcd652
Giulia Abate <giulia.abate1101@gmail.com>
2025-10-05 16:36:35 +0200

    navbar + home responsive

--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,8 +10,10 @@ import './NavigationBar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavigationBar() {
+    const [showSidebar, setShowSidebar] = useState(false);
+    const toggleSidebar = () => setShowSidebar(!showSidebar);
+
     return (
-        //fixed="top"
         <Navbar expand="xl" className="custom-navbar">
             <Container>
                 <Navbar.Brand as={Link} to="/">
@@ -22,8 +25,14 @@ function NavigationBar() {
                         alt="veriflix logo"
                     />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
+
+                {/* Hamburger menu */}
+                <button className="navbar-toggler" type="button" onClick={toggleSidebar}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                {/* Nav standard per desktop */}
+                <Navbar.Collapse id="responsive-navbar-nav" className="desktop-nav">
                     <Nav>
                         <Nav.Link as={Link} to="/originals">Originals</Nav.Link>
                         <Nav.Link as={Link} to="/music-videos">Music Videos</Nav.Link>
@@ -31,17 +40,28 @@ function NavigationBar() {
                         <Nav.Link as={Link} to="/entertainment">Entertainment</Nav.Link>
                         <Nav.Link as={Link} to="/performances">Performances</Nav.Link>
                         <Navbar.Brand as={Link} to="/cast">
-                            <img
-                                src={icon}
-                                width={80}
-                                height={80}
-                                className='icon d-inline-block align-items-center ms-5 me-3'
-                                alt="verivery"
-                            />
+                            <img src={icon} width={80} height={80} className='icon ms-5 me-3' alt="verivery"/>
                             <span className="brand-text">Cast</span>
                         </Navbar.Brand>
                     </Nav>
                 </Navbar.Collapse>
+
+                {/* Sidebar solo per mobile */}
+                <div className={`sidebar ${showSidebar ? 'show' : ''}`}>
+                    <Nav className="flex-column">
+                        <Nav.Link as={Link} to="/originals" onClick={toggleSidebar}>Originals</Nav.Link>
+                        <Nav.Link as={Link} to="/music-videos" onClick={toggleSidebar}>Music Videos</Nav.Link>
+                        <Nav.Link as={Link} to="/discography" onClick={toggleSidebar}>Discography</Nav.Link>
+                        <Nav.Link as={Link} to="/entertainment" onClick={toggleSidebar}>Entertainment</Nav.Link>
+                        <Nav.Link as={Link} to="/performances" onClick={toggleSidebar}>Performances</Nav.Link>
+                        <Nav.Link as={Link} to="/cast" onClick={toggleSidebar}>
+                            <img src={icon} width={80} height={80} className='icon me-2' alt="verivery"/>
+                            Cast
+                        </Nav.Link>
+                    </Nav>
+                </div>
+
+                {showSidebar && <div className="overlay" onClick={toggleSidebar}></div>}
             </Container>
         </Navbar>
     );
